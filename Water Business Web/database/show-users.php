209fd06408afb64806020ab381bc1735ff3cb563
RC1010 <91231779+RC1010@users.noreply.github.com>
2025-02-24 18:13:58 +0800

    Delete Water Business Web directory

--- Water Business Web/database/show-users.php
+++
@@ -1,9 +0,0 @@
-<?
-include('connection.php');
-
-$stmt = $conn->prepare("SELECT * FROM user ORDER BY created_at DESC");
-$stmt->execute();
-$stmt->setFetchMode(PDO::FETCH_ASSOC);
-
-return $stmt->fetchAll();
-?>
