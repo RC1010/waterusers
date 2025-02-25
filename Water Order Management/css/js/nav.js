2362f1dd382b494d510540cc126e5e31ddcf6949
RC1010 <91231779+RC1010@users.noreply.github.com>
2025-02-25 21:07:05 +0800

    Add files via upload

---
+++ Water Order Management/css/js/nav.js
@@ -0,0 +1,29 @@
+const dropdownBtn = document.getElementById("btn");
+const dropdownMenu = document.getElementById("dropdown");
+const toggleArrow = document.getElementById("arrow");
+
+const toggleDropdown = function () {
+    dropdownMenu.classList.toggle("show");
+    toggleArrow.classList.toggle("arrow");
+  };
+
+  dropdownBtn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    toggleDropdown();
+  });
+
+//* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
